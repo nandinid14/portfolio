8406d9847e857e7d3e42b6c626068a5270d4f4cb
snehaprajapati03 <snehaprajapati149@gmail.com>
2024-11-29 09:26:47 +0530

    index.html

--- my.js
+++
@@ -1,9 +0,0 @@
-console.log("helo me!")
-a=8
-b=10
-console.log("sum of a+b is:",+(a+b))
-str= "siara here!"
-str2= 'siara wants to know you...'
-str3= 'are you there "siara"'
-console.log(str3)
-console.log(str2)
